39d804be417ac762aab65d6fed29ff1f34311a14
zackjheil <43187743+zackjheil@users.noreply.github.com>
2018-11-20 18:24:52 -0500

    ASFSDg

---
+++ November20.js
@@ -0,0 +1,34 @@
+
+var Counting = new Array();
+Counting[0] = ("1");
+  Counting.push("<br/>");
+Counting.push("2");
+  Counting.push("<br/>");
+Counting.push("3");
+  Counting.push("<br/>");
+Counting.push("4");
+  Counting.push("<br/>");
+Counting.push("5");
+  Counting.push("<br/>");
+Counting.push("6");
+  Counting.push("<br/>");
+Counting.push("7");
+  Counting.push("<br/>");
+Counting.push("8");
+  Counting.push("<br/>");
+Counting.push("9");
+  Counting.push("<br/>");
+Counting.push("10");
+  Counting.push("<br/>");
+Counting.push("11");
+  Counting.push("<br/>");
+Counting.push("12");
+  Counting.push("<br/>");
+
+for (i=0;i<=0;i++){
+
+document.write(Counting);
+}
+
+
+//myArray.push("2");
